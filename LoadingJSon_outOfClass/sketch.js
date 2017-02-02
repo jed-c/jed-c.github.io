977f7bd314bda40189f726965ce9b85cac443be1
Jed Crocker <jedcrocker@Jeds-MacBook-Air.local>
2017-02-02 00:17:51 -0500

    adding LoadingJSon_outofClass

---
+++ LoadingJSon_outOfClass/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // load the colors.json file
+    loadJSON('colors.json', showData);
+    textAlign(CENTER);
+
+}
+
+function showData(data) {
+    fill(data.green);
+    textSize(300);
+    text(data.yellow, width/2, height/2);
+}
+
+   	console.log("seriously wtf WORLD");
